8d8aec7432aa6480804aac204b994f8a97dc2636
Keith Smith <aeiouandsometimesy@me.com>
2017-10-06 14:46:20 -0400

    Initial version tested and working.

--- PRG-3-1-Miles-per-Gallon/main.cpp
+++ PRG-3-1-Miles-per-Gallon/main.cpp
@@ -1,15 +1,33 @@
-//
-//  main.cpp
-//  PRG-3-1-Miles-per-Gallon
-//
-//  Created by Keith Smith on 10/6/17.
-//  Copyright © 2017 Keith Smith. All rights reserved.
-//
-
+// This program calculates miles per gallon (MPG)
+// from inputs: total distance, total number of gallons
+// a car can travel, that a car can hold.
 #include <iostream>
+#include <iomanip>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    int intTotalDistance;
+    int intTotalGallons;
+    
+    float fltTotalMPG;
+    
+    cout << "Please enter the maximum distance your car can travel: " << endl;
+    cin >> intTotalDistance;
+    
+    cout << "Please enter the size of you gas tank in gallons: " << endl;
+    cin >> intTotalGallons;
+    
+    fltTotalMPG = static_cast<float>(intTotalDistance) / static_cast<float>(intTotalGallons);
+    
+    cout << "The MPG of your car is: " << endl;
+    cout << setprecision(1) << fixed << fltTotalMPG; //Set decimal carry-over to single digit and
+    //use fixed to display decimel instead of scientific
+    
     return 0;
 }
+
+
+
+
